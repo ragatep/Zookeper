bbe09037c59c51a7075fa2ff47dcb181d570addc
ragatep <ragatep@gmail.com>
2023-02-05 19:00:51 -0800

    Initial commit

---
+++ Rush_Into_Print/RushIntoPrint.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        /** Print the following in its own line:
+         * "I love animals!"
+         * "Let's check on the animals..."
+         * "The deer looks fine."
+         * "The bat looks happy."
+         * "The lion looks healthy."
+         */
+        System.out.println("I love animals!");
+        System.out.println("Let's check on the animals...");
+        System.out.println("The deer looks fine.");
+        System.out.println("The bat looks happy.");
+        System.out.println("The lion looks healthy.");
+    }
+}
