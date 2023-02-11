71e6a315537818c0aeb4c4f84ee055850a0ca844
ragatep <ragatep@gmail.com>
2023-02-10 19:46:41 -0800

    Stage 3/4: What's inside? Initial Commit

---
+++ Whats_Inside/WhatsInside.java
@@ -0,0 +1,178 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""";
+
+        String lion = """
+            Switching on the camera in the lion habitat...
+                                                           ,w.
+                                                         ,YWMMw  ,M  ,
+                                    _.---.._   __..---._.'MMMMMw,wMWmW,
+                               _.-""        '''           YP"WMMMMMMMMMb,
+                            .-' __.'                   .'     MMMMW^WMMMM;
+                _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+             ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+            ,MM:.    .'.-'   .'     ;     `\\    ;     `,     MMMMMMMW `"=./`-,
+            WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+            "^MP__.-'    ,-' _.--""   `-,   ;       \\  ; ;MMMMMMMMMMW^``; __|
+                       /   .'            ; ;         )  )`{  \\ `"^W^`,   \\  :
+                      /  .'             /  (       .'  /     Ww._     `.  `"
+                     /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+                    (--, )                `,_ / `) \\/"")      ^"      `-, -;"\\:
+            The lion is roaring!""";
+
+        String deer = """
+            Switching on the camera in the deer habitat...
+               /|       |\\
+            `__\\       //__'
+               ||      ||
+             \\__`\\     |'__/
+               `_\\   //_'
+               _.,:---;,._
+               \\_:     :_/
+                 |@. .@|
+                 |     |
+                 ,\\.-./ \\
+                 ;;`-'   `---__________-----.-.
+                 ;;;                         \\_\\
+                 ';;;                         |
+                  ;    |                      ;
+                   \\   \\     \\        |      /
+                    \\_, \\    /        \\     |\\
+                      |';|  |,,,,,,,,/ \\    \\ \\_
+                      |  |  |           \\   /   |
+                      \\  \\  |           |  / \\  |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       |_||_|           |_|   |_|
+                      /_//_/           /_/   /_/
+            Our 'Bambi' looks hungry. Let's go to feed it!""";
+
+        String goose = """
+            Switching on the camera in the goose habitat...
+            
+                                                _
+                                            ,-"" "".
+                                          ,'  ____  `.
+                                        ,'  ,'    `.  `._
+               (`.         _..--.._   ,'  ,'        \\    \\
+              (`-.\\    .-""        ""'   /          (  d _b
+             (`._  `-"" ,._             (            `-(   \\
+             <_  `     (  <`<            \\              `-._\\
+              <`-       (__< <           :
+               (__        (_<_<          ;
+                `------------------------------------------
+            The goose is staring intently at you... Maybe it's time to change the channel?""";
+
+        String bat = """
+            Switching on the camera in the bat habitat...
+            _________________               _________________
+             ~-.              \\  |\\___/|  /              .-~
+                 ~-.           \\ / o o \\ /           .-~
+                    >           \\  W  //           <
+                   /             /~---~\\             \\
+                  /_            |       |            _\\
+                     ~-.        |       |        .-~
+                        ;        \\     /        i
+                       /___      /\\   /\\      ___\\
+                            ~-. /  \\_/  \\ .-~
+                               V         V
+            This bat looks like it's doing fine.""";
+
+        String rabbit = """
+            Switching on the camera in the rabbit habitat...
+                     ,
+                    /|      __
+                   / |   ,-~ /
+                  Y :|  //  /
+                  | jj /( .^
+                  >-"~"-v"
+                 /       Y
+                jo  o    |
+               ( ~T~     j
+                >._-' _./
+               /   "~"  |
+              Y     _,  |
+             /| ;-"~ _  l
+            / l/ ,-"~    \\
+            \\//\\/      .- \\
+             Y        /    Y
+             l       I     !
+             ]\\      _\\    /"\\
+            (" ~----( ~   Y.  )
+            It looks like we will soon have more rabbits!""";
+
+        /** Import Scanner class and create a scanner object
+         * Create an array of Strings(String[] animals = {String block of code for each animal})
+         * Print the prompt "Please enter the number of the habitat you would like to view: "
+         * Accept user input nextInt() from the Scanner object
+         * Print the data saved in the array at the index of the user input.
+         * End the text block with the following phrase:
+         * ---
+         * You've reached the end of the program. To check another habitat, please restart the watcher.
+         * */
+        Scanner scanner = new Scanner(System.in);
+        String[] animals = {camel, lion, deer, goose, bat, rabbit};
+        while (true) {
+            System.out.println("Please enter the number of the habitat you would like to view: ");
+            int input = scanner.nextInt();
+            if (input == 0) {
+                System.out.println(animals[0] + """
+                        ---
+                        You've reached the end of the program. To check another habitat, please restart the watcher.
+                        """);
+
+            } else if (input == 1) {
+                System.out.println(animals[1] + """ 
+                        ---
+                        You've reached the end of the program. To check another habitat, please restart the watcher.
+                        """);
+            } else if (input == 2) {
+                System.out.println(animals[2] + """
+                        ---
+                        You've reached the end of the program. To check another habitat, please restart the watcher.
+                        """);
+            } else if (input == 3) {
+                System.out.println(animals[3] + """
+                        ---
+                        You've reached the end of the program. To check another habitat, please restart the watcher.
+                        """);
+            } else if (input == 4) {
+                System.out.println(animals[4] + """
+                        ---
+                        You've reached the end of the program. To check another habitat, please restart the watcher.
+                        """);
+            } else if (input == 5) {
+                System.out.println(animals[5] + """
+                        ---
+                        You've reached the end of the program. To check another habitat, please restart the watcher.
+                        """);
+            } else if (input == 6) {
+                System.out.println("You've reached the end of the program. To check another habitat, please restart the watcher.");
+                break;
+            }
+        }
+
+    }
+}
